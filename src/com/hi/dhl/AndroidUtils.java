8c61d5d6149fc17de6854207e4508c606a9897c1
jack <pmdhl100@gmail.com>
2019-01-14 14:49:04 +0800

    feat: 添加启动Android Device Monitor

---
+++ src/com/hi/dhl/AndroidUtils.java
@@ -0,0 +1,49 @@
+package com.hi.dhl;
+
+
+import com.intellij.openapi.project.Project;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2019/1/14
+ *     desc  : 获取Android配置信息的工具类
+ * </pre>
+ */
+public class AndroidUtils {
+
+    /**
+     * sdk 配置的路径
+     * @param project
+     * @return
+     */
+    public static String getApkLocalProperties(Project project){
+        String sdkPath = "";
+       try {
+           String path = project.getBasePath() + File.separator + "local.properties";
+
+           Properties properties = new Properties();
+           InputStream inputStream = new FileInputStream(path);
+           properties.load( inputStream );
+
+           sdkPath = properties.getProperty("sdk.dir");
+       }catch (Exception e){
+
+       }
+        return sdkPath;
+    }
+
+    /**
+     * 获取不同平台的 win、mac etc;
+     * @return
+     */
+    public static String getPlatformName(){
+        String osName = System.getProperty("os.name");
+        return osName;
+    }
+}
---
+++ src/com/hi/dhl/Monitor.java
@@ -0,0 +1,37 @@
+package com.hi.dhl;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+
+import java.io.File;
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2019/1/14
+ *     desc  : 插件启动入口
+ * </pre>
+ */
+public class Monitor extends AnAction{
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        try {
+            Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
+
+            String os = AndroidUtils.getPlatformName();
+            String sdkPath = AndroidUtils.getApkLocalProperties(project);
+            if (os.toLowerCase().startsWith("win")) {
+                sdkPath  += File.separator+"tools"+File.separator+"monitor.bat";
+            } else {
+                sdkPath += File.separator+"tools"+File.separator+"monitor";
+            }
+
+            Runtime.getRuntime().exec(sdkPath);
+        } catch (Exception e) {
+
+        }
+
+    }
+}
